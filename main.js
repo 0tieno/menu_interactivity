81803061df0cf68b3d4e824623418986ee5699ec
Otieno <ronvi035@gmail.com>
2024-01-22 18:37:19 +0300

    factored js code

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-document.querySelector(".menu-btn").addEventListener("click", () => {
-  document.querySelector(".nav-menu").classList.toggle("show");
-});
+// document.querySelector(".menu-btn").addEventListener("click", () => {
+//   document.querySelector(".nav-menu").classList.toggle("show");
+// });
+
+let menuBtn = document.querySelector(".menu-btn");
+let navMenu = document.querySelector(".nav-menu");
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  navMenu.classList.toggle("show");
+}
+
+
+
